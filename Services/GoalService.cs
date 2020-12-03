d1d74df05ff4c592f43755472cf4673838cc2662
Simona <Simona@ZILVINO-WINDOWS>
2020-12-03 19:23:29 +0200

    fixed Insights method

--- Services/GoalService.cs
+++ Services/GoalService.cs
@@ -33,14 +33,21 @@ namespace Plutus.WebService
 
         public string Insights(Goal goal, string dailyOrMonthly)
         {
-            var monthlyIncome = _fileManager.ReadPayments(DataType.MonthlyIncome);
-            var monthlyExpenses = _fileManager.ReadPayments(DataType.MonthlyExpenses);
+            var scheduledIncome = _fileManager.LoadScheduledPayments(DataType.MonthlyIncome);
+            var scheduledExpenses = _fileManager.LoadScheduledPayments(DataType.MonthlyExpenses);
             var allIncome = _fileManager.ReadPayments(DataType.Income);
             var allExpenses = _fileManager.ReadPayments(DataType.Expense);
 
             var months = goal.DueDate.Month - DateTime.Now.Month + (12 * (goal.DueDate.Year - DateTime.Now.Year));
-            var income = monthlyIncome.Sum(x => x.Amount * months) + allIncome.Sum(x => x.Amount);
-            var expenses = monthlyExpenses.Sum(x => x.Amount * months) + allExpenses.Sum(x => x.Amount);
+            var weeks = (int)(goal.DueDate - DateTime.Now).TotalDays / 7;
+
+            var weeklyIncome = scheduledIncome.Where(x => x.Active == true).Where(x => x.Frequency == "Weekly").Sum(x => x.Amount * weeks);
+            var weeklyExpenses = scheduledExpenses.Where(x => x.Active == true).Where(x => x.Frequency == "Weekly").Sum(x => x.Amount * weeks);
+            var monthlyIncome = scheduledIncome.Where(x => x.Active == true).Where(x => x.Frequency == "Monthly").Sum(x => x.Amount * months);
+            var monthlyExpenses = scheduledExpenses.Where(x => x.Active == true).Where(x => x.Frequency == "Monthly").Sum(x => x.Amount * months);
+
+            var income = weeklyIncome + monthlyIncome + allIncome.Sum(x => x.Amount);
+            var expenses = weeklyExpenses + monthlyExpenses + allExpenses.Sum(x => x.Amount);
 
             var todaySpent = allExpenses.Where(x => x.Date.ConvertToDate().ToString("yyyy/MM/dd") == DateTime.Now.ToString("yyyy/MM/dd")).Sum(x => x.Amount);
 
