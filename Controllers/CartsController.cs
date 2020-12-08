49fd6f8d957f248afc1264c6ac5106ec0b73b11e
Mindaugas-Ko <71326991+Mindaugas-Ko@users.noreply.github.com>
2020-12-08 11:36:43 +0200

    Cart Controller changed

--- Controllers/CartsController.cs
+++ Controllers/CartsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
 
 namespace Plutus.WebService.Controllers
 {
@@ -6,86 +7,13 @@ namespace Plutus.WebService.Controllers
     [ApiController]
     public class CartsController : ControllerBase
     {
-        private readonly CartService _cartService = new CartService();
+        private FileManager _fm = new FileManager();
 
-        [HttpGet("LoadMessage")]
-        public string GiveLoadMessage()
-        {
-            return _cartService.GiveLoadMessage();
-        }
+        [HttpGet("LoadCarts")]
+        public XElement LoadCarts() => _fm.LoadCarts();
 
-        [HttpGet("CartCount")]
-        public int GiveCartCount()
-        {
-            return _cartService.GiveCartCount();
-        }
-
-        [HttpGet("{index}/CartName")]
-        public string GiveNameAt(int index)
-        {
-            return _cartService.GiveCartNameAt(index);
-        }
-
-        [HttpGet("CurrentCartExpenseCount")]
-        public int GiveExpenseCount()
-        {
-            return _cartService.GiveCurrentCartElemCount();
-        }
-
-        [HttpGet("CartExpenseAt/{index}")]
-        public CartExpense GiveExpenseAt(int index)
-        {
-            return _cartService.GiveCurrentElemAt(index);
-        }
-
-        [HttpPost("{index}/SelectCart")]
-        public void CartSelected(int index)
-        {
-            _cartService.CurrentCartSet(index);
-        }
-
-        [HttpPost("NewCart")]
-        public void NewCart()
-        {
-            _cartService.NewCart();
-        }
-
-        [HttpPost("AddExpense")]
-        public void AddExpense(CurrentInfoHolder _cih)
-        {
-            _cartService.AddExpenseToCart(_cih);
-        }
-
-        [HttpPost("SaveCart")]
-        public void SaveNewCart(string name, int prevCart)
-        {
-            _cartService.SetCurrentName(name);
-            _cartService.SaveCartChanges(prevCart);
-        }
-
-        [HttpDelete("DeleteCurrentCart")]
-        public void DeleteCart()
-        {
-            _cartService.DeleteCurrent();
-        }
-
-        [HttpPost("ChargeCurrentCart")]
-        public void ChargeCart()
-        {
-            _cartService.ChargeCart();
-        }
-
-        [HttpDelete("{index}/DeleteCart")]
-        public void RemoveExpense(int index)
-        {
-            _cartService.RemoveExpenseCurrentAt(index);
-        }
-
-        [HttpPost("ChargeShopping")]
-        public void ChargeShopping(ShoppingService service)
-        {
-            service.ChargeShopping();
-        }
+        [HttpPost("SaveCarts")]
+        public void SaveCarts(XElement carts) => _fm.SaveCarts(carts);
 
     }
 }
