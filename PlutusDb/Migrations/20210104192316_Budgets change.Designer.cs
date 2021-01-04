fbc0b777169d0d1d23d5316a3b4fb052a4b871a3
RokasBagdonas <r.bagdonas2000@gmail.com>
2021-01-04 21:23:37 +0200

    .

--- PlutusDb/Migrations/20210104192316_Budgets change.Designer.cs
+++ PlutusDb/Migrations/20210104192316_Budgets change.Designer.cs
@@ -4,14 +4,16 @@ using Db;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace PlutusDb.Migrations
 {
     [DbContext(typeof(SmartsaverDbContext))]
-    partial class SmartsaverDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210104192316_Budgets change")]
+    partial class Budgetschange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ PlutusDb/Migrations/20210104192316_Budgets change.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PlutusDb.Migrations
+{
+    public partial class Budgetschange : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
