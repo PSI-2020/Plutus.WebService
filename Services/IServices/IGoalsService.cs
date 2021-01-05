cc2d260b10685ee30a1e347b07dc3045ae42476f
RokasBagdonas <r.bagdonas2000@gmail.com>
2021-01-05 12:56:04 +0200

    linking goals to db

--- Services/IServices/IGoalsService.cs
+++ Services/IServices/IGoalsService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Plutus.WebService.IRepos
 {
@@ -8,5 +9,7 @@ namespace Plutus.WebService.IRepos
         public void DeleteGoal(Goal goal);
         public void SetMainGoal(Goal goal);
         public string Insights(Goal goal, string dailyOrMonthly);
+        public List<Goal> GetGoalsList();
+        public void AddGoal(Goal goal);
     }
 }
